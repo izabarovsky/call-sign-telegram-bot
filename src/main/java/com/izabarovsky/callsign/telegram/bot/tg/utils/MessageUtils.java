3d9f2ee769cfc23e0331e1be8faccbca4e70ed19
Ihor Zabarovskyi <i.zabarovskyi@steel.mil.ua>
2024-06-19 13:10:44 +0300

    [CS-33] Refactoring message utils

--- src/main/java/com/izabarovsky/callsign/telegram/bot/tg/utils/MessageUtils.java
+++ src/main/java/com/izabarovsky/callsign/telegram/bot/tg/utils/MessageUtils.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
 import java.nio.charset.Charset;
 import java.time.ZoneId;
@@ -15,45 +16,24 @@ import java.util.List;
 import java.util.Objects;
 
 import static com.izabarovsky.callsign.telegram.bot.tg.utils.MenuUtils.*;
-import static com.izabarovsky.callsign.telegram.bot.tg.utils.TextUtils.textStatistics;
+import static com.izabarovsky.callsign.telegram.bot.tg.utils.TextUtils.*;
 import static java.util.Objects.nonNull;
 
 public class MessageUtils {
 
     public static HandlerResult msgGroupMyK2Info(Long chatId, Integer threadId, CallSignModel callSignModel) {
         String payload = parseMyCallSign(callSignModel);
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildMainMenu())
-                .text(payload)
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, payload, buildMainMenu());
     }
 
     public static HandlerResult msgPrivateMyK2Info(Long chatId, Integer threadId, CallSignModel callSignModel) {
         String payload = parseMyCallSign(callSignModel);
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildEditInlineMenu())
-                .text(payload)
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, payload, buildEditInlineMenu());
     }
 
     public static HandlerResult msgK2Info(Long chatId, Integer threadId, CallSignModel callSignModel) {
         String payload = parseCallSign(callSignModel);
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildMainMenu())
-                .text(payload)
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, payload, buildMainMenu());
     }
 
     public static HandlerResult msgStatistics(Long chatId, Integer threadId, List<CallSignModel> list) {
@@ -61,24 +41,11 @@ public class MessageUtils {
         long official = list.stream().filter(s -> nonNull(s.getOfficialCallSign())).count();
         long dmr = list.stream().filter(s -> nonNull(s.getDmrId())).count();
         long nonOfficial = total - official;
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildMainMenu())
-                .text(textStatistics(total, official, nonOfficial, dmr))
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textStatistics(total, official, nonOfficial, dmr), buildMainMenu());
     }
 
     public static HandlerResult msgNewcomer(Long chatId, Integer threadId, String userName) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .replyMarkup(buildCreateMenu())
-                .text(TextUtils.textHelloNewcomer(userName))
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textHelloNewcomer(userName), buildCreateMenu());
     }
 
     public static HandlerResult msgEnterValueRequired(Long chatId) {
@@ -90,79 +57,36 @@ public class MessageUtils {
     }
 
     public static HandlerResult msgOnAnyUnknown(Long chatId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildMainMenu())
-                .text(TextUtils.textUseMenuButtons())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textUseMenuButtons(), buildMainMenu());
     }
 
     public static HandlerResult msgEnterValueOrSkip(Long chatId, String payload) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildSkipOrCancelMenu())
-                .text(TextUtils.textEnterValueOrSkip(payload))
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textEnterValueOrSkip(payload), buildSkipOrCancelMenu());
     }
 
     public static HandlerResult msgEnterSearchOrCancel(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .replyMarkup(buildCancelMenu())
-                .text(TextUtils.textEnterSearch())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textEnterSearch(), buildCancelMenu());
     }
 
     public static HandlerResult msgCantSkip(Long chatId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildCancelMenu())
-                .text(TextUtils.textStepCantSkip())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textStepCantSkip(), buildCancelMenu());
     }
 
     public static HandlerResult msgCallSingIsBooked(Long chatId, String callSign) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildSkipOrCancelMenu())
-                .text(TextUtils.textCallSingIsBooked(callSign))
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textCallSingIsBooked(callSign), buildSkipOrCancelMenu());
     }
 
     public static HandlerResult msgCallSingIsInvalid(Long chatId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildSkipOrCancelMenu())
-                .text(TextUtils.textCallSingIsInvalid())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textCallSingIsInvalid(), buildSkipOrCancelMenu());
     }
 
     public static HandlerResult msgDialogDone(Long chatId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .replyMarkup(buildMainMenu())
-                .text(TextUtils.textDialogDone())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, null, textDialogDone(), buildMainMenu());
     }
 
     public static HandlerResult msgSearchResult(Long chatId, Integer threadId, List<CallSignModel> list) {
         String text = list.isEmpty() ? TextUtils.textNothingFound() : parseList(list);
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .replyMarkup(buildCancelMenu())
-                .parseMode(ParseMode.HTML)
-                .text(text)
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, text, buildCancelMenu());
     }
 
     public static SendDocument msgOnGetAll(Long chatId, Integer threadId, String payload) {
@@ -178,25 +102,11 @@ public class MessageUtils {
     }
 
     public static HandlerResult msgK2InfoNotFound(Long chatId, Integer threadId, String username) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildMainMenu())
-                .text(TextUtils.textUserNotFound(username))
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textUserNotFound(username), buildMainMenu());
     }
 
     public static HandlerResult msgK2InfoHowTo(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildMainMenu())
-                .text(TextUtils.textUseK2InfoCommandAsFollow())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textUseK2InfoCommandAsFollow(), buildMainMenu());
     }
 
     public static SendMessage msgCongratsDmrIdMsg(String chatId, String threadId, CallSignModel callSign) {
@@ -210,44 +120,19 @@ public class MessageUtils {
     }
 
     public static HandlerResult msgRepeaters(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .replyMarkup(buildRepeatersInlineMenu())
-                .text(TextUtils.textRepeaters())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textRepeaters(), buildRepeatersInlineMenu());
     }
 
     public static HandlerResult msgRepeatersNonOfficial(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .text(TextUtils.textRepeatersNonOfficial())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textRepeatersNonOfficial(), null);
     }
 
     public static HandlerResult msgRepeatersOfficial(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .text(TextUtils.textRepeatersOfficial())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textRepeatersOfficial(), null);
     }
 
     public static HandlerResult msgRepeatersEcholink(Long chatId, Integer threadId) {
-        var msg = SendMessage.builder()
-                .chatId(chatId)
-                .messageThreadId(threadId)
-                .parseMode(ParseMode.HTML)
-                .text(TextUtils.textRepeatersEcholink())
-                .build();
-        return new HandlerResult(msg);
+        return newMessage(chatId, threadId, textRepeatersEcholink(), null);
     }
 
     public static String parseList(List<CallSignModel> list) {
@@ -285,6 +170,16 @@ public class MessageUtils {
                 callSignModel.getDmrId(),
                 formatter.format(callSignModel.getCreationTimestamp())
         );
+    }
+
+    private static HandlerResult newMessage(Long chatId, Integer threadId, String text, ReplyKeyboard replyMarkup) {
+        var msg = SendMessage.builder()
+                .chatId(chatId)
+                .messageThreadId(threadId)
+                .parseMode(ParseMode.HTML)
+                .text(text)
+                .build();
+        return new HandlerResult(msg);
     }
 
 }
